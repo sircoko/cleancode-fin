be3ceb9611607d7adcaf752f926542241e5d75ea
Jose Camacho <jrcamachoc@gmail.com>
2023-09-07 23:03:20 -0400

    Added port log

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,12 @@ import { AppModule } from './app.module';
 // import * as fs from 'fs';
 
 async function bootstrap() {
-  const httpsOptions = {
-    // key: fs.readFileSync('./secrets/server.key'),
-    // cert: fs.readFileSync('./secrets/server.crt'),
-  };
-  const app = await NestFactory.create(AppModule, {
-    httpsOptions,
-  });
+  // const httpsOptions = {
+  //   // key: fs.readFileSync('./secrets/server.key'),
+  //   // cert: fs.readFileSync('./secrets/server.crt'),
+  // };
+  const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT || 3000);
+  console.log('Running on Port', process.env.PORT);
 }
 bootstrap();
