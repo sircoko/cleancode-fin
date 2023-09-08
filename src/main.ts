b9a20824206122bcc8662f6f7fd1cdb4b7b27145
Jose Camacho <jrcamachoc@gmail.com>
2023-09-07 22:23:14 -0400

    added heroku files

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+// import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    // key: fs.readFileSync('./secrets/server.key'),
+    // cert: fs.readFileSync('./secrets/server.crt'),
+  };
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
   await app.listen(3000);
 }
 bootstrap();
